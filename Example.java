91618d42702b37e3624448abdd6d159672b9ed45
mostafa12345 <hosseinzade2731@gmail.com>
2016-12-10 17:07:50 +0330

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,14 @@
+/**
+* @author mostafa hosseinzade
+**/
+public class Example {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ConvertToCharPersion c = new ConvertToCharPersion();
+        System.out.println(c.num2str("253456"));
+    }
+    
+}
